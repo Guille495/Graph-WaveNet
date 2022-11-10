0dd2d7964ef4811a85c32568555a9258c5a849dc
Guillermo Pereira <guillermo.pereira@pedidosya.com>
2022-11-10 19:46:03 -0300

    Update train.py
    Minor for loop sequence fix

--- train.py
+++ train.py
@@ -152,7 +152,7 @@ def main():
     amae = []
     amape = []
     armse = []
-    for i in [0 , args.seq_length]:
+    for i in [0 , args.seq_length -1]:
         pred = scaler.inverse_transform(yhat[:,:,i])
         real = realy[:,:,i]
         metrics = util.metric(pred,real)
