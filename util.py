8d6ab1eef8faf491bda78fd42190f49b64e0b73d
Guillermo Pereira <guillermopereira495@gmail.com>
2023-10-03 02:13:07 -0300

    Further adjustments to move Numpy objects to CPU ancillary

--- util.py
+++ util.py
@@ -20,10 +20,10 @@ class DataLoader(object):
         self.current_ind = 0
         if pad_with_last_sample:
             num_padding = (batch_size - (len(xs) % batch_size)) % batch_size
-            x_padding = np.repeat(xs[-1:], num_padding, axis=0)
-            y_padding = np.repeat(ys[-1:], num_padding, axis=0)
-            xs = np.concatenate([xs, x_padding], axis=0)
-            ys = np.concatenate([ys, y_padding], axis=0)
+            x_padding = np.repeat(xs[-1:].cpu(), num_padding, axis=0)
+            y_padding = np.repeat(ys[-1:].cpu(), num_padding, axis=0)
+            xs = np.concatenate([xs.cpu(), x_padding], axis=0)
+            ys = np.concatenate([ys.cpu(), y_padding], axis=0)
         self.size = len(xs)
         self.num_batch = int(self.size // self.batch_size)
         self.xs = xs
@@ -179,7 +179,7 @@ def load_dataset(dataset_dir, batch_size, valid_batch_size= None, test_batch_siz
         data[f'test_y_fold_{i}'] = [data['y_crossval'][j] for j in test_index]
         data[f'test_fold_{i}_loader'] = DataLoader(data[f'test_x_fold_{i}'], data[f'test_y_fold_{i}'], batch_size, data['dates_crossval'], data['stations_crossval'])
 
-    scaler = StandardScaler(mean=data['x_train'][..., 0].mean(), std=data['x_train'][..., 0].std())
+    scaler = StandardScaler(mean=data['x_train'][..., 0].cpu().mean(), std=data['x_train'][..., 0].std())
     # Data format
     for category in ['train', 'val', 'test']:
         data['x_' + category][..., 0] = scaler.transform(data['x_' + category][..., 0])
