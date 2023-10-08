93a53dc26445a97f578ea218596c38c4d7330425
Guillermo Pereira <guillermopereira495@gmail.com>
2023-10-08 18:21:30 -0300

    Update util.py

--- util.py
+++ util.py
@@ -20,10 +20,10 @@ class DataLoader(object):
         self.current_ind = 0
         if pad_with_last_sample:
             num_padding = (batch_size - (len(xs) % batch_size)) % batch_size
-            x_padding = np.repeat(xs[-1:].numpy().cpu(), num_padding, axis=0)
-            y_padding = np.repeat(ys[-1:].numpy().cpu(), num_padding, axis=0)
-            xs = np.concatenate([xs, x_padding], axis=0)
-            ys = np.concatenate([ys, y_padding], axis=0)
+            x_padding = np.repeat(xs[-1:], num_padding, axis=0)
+            y_padding = np.repeat(ys[-1:], num_padding, axis=0)
+            xs = np.concatenate([xs, x_padding], axis=0).cpu()
+            ys = np.concatenate([ys, y_padding], axis=0).cpu()
         self.size = len(xs)
         self.num_batch = int(self.size // self.batch_size)
         self.xs = xs
