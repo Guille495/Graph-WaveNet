2926f0c815997602879d43c0be7040b5dda7678e
Guillermo Pereira <guillermopereira495@gmail.com>
2024-08-31 17:31:13 -0300

    Edited pritn()

--- test.py
+++ test.py
@@ -163,7 +163,7 @@ def save_predictions(realy, yhat, scaler, args, variant, addaptadj_text):
     print(f'Shape is {y_real.shape[0]} real values , {y_hat.shape[0]} predictions , {y_seq_length.shape[0]} timesteps , {temporal_horizon.shape[0]} replicated timesteps , {sensor_yrealy.shape[0]} rows per sensor (timesteps * horizons) , {sensor_id.shape[0]} repeated sensors')
     print()
     print()
-    print(f'Sensor_id: {sensor_id.size} , Temporal horizon: {temporal_horizon.size}, Tile: {tile.size}, Y real: {y_real.size}, Y hat: {y_hat.size}')
+    print(f'Sensor_id: {sensor_id.size} , Temporal horizon: {temporal_horizon.size}, Timesteps: {timesteps.size}, Y real: {y_real.size}, Y hat: {y_hat.size}')
     print()
     print()
     
