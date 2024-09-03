bd1572fb89ceef81ea9b062a06f250c7baa6dce9
Guillermo Pereira <guillermopereira495@gmail.com>
2024-09-03 00:33:07 -0300

    Changed file output name

--- test.py
+++ test.py
@@ -183,7 +183,7 @@ def save_predictions(realy, yhat, scaler, args, variant, addaptadj_text):
     print()
     
     df2 = pd.DataFrame({'sensor id': sensors, 'temporal horizon': temporal_horizon, 'timesteps': timesteps, 'real_values': y_real, 'pred_values': y_hat})
-    df2.to_csv(f'./predictions_{variant}_{addaptadj_text}.csv', index=False)
+    df2.to_csv(f'./predictions_{variant}_{addaptadj_text}_{args.prediction_multi_or_single}.csv', index=False)
 
 
 if __name__ == "__main__":
